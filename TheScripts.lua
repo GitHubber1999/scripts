23bdbeef632b3453ce6a421f27f9a46e31b22f79
GitHubber1999 <100528000+GitHubber1999@users.noreply.github.com>
2022-04-17 23:28:34 +0100

    Create TheScripts.lua

---
+++ TheScripts.lua
@@ -0,0 +1,32 @@
+-- Da Hood BetaGui [ It's a GUI with scripts in it; like RAYX, Faded, etc. ]
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/GitHubber1999/betaguiob/main/script.lua"))()
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+ -- BEST FPS BOOST SCRIPT [ PS1 GRAPHICS ]
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/GitHubber1999/fpsboost/main/script.lua"))()
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+
+-- Da Hood GS21 Seller Tools
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GS21Official/-ToolsScript/main/!Tools%20script%20page'))() 
+
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+-- Da Hood Gui V.1
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/GitHubber1999/gui-v1.1/main/script.lua"))()
+
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+-- More to be added!
